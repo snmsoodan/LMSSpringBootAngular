520c53b95670a80398a05d5d5c3bfea8208d5511
sanamdeep singh <sanamsoodan@gmail.com>
2018-06-18 11:57:26 -0400

    aws dockerservices

--- LMSSpringBootAngular/src/main/resources/static/common/lms-constants.js
+++ LMSSpringBootAngular/src/main/resources/static/common/lms-constants.js
@@ -1,56 +1,56 @@
 lmsApp.constant("lmsConstants",{
-	GET_ALL_AUTHORS: "http://localhost:8084/lms/authors",
-	DELETE_AUTHOR: "http://localhost:8084/lms/authors/",
-	UPDATE_AUTHOR: "http://localhost:8084/lms/authors/",
-	ADD_AUTHOR: "http://localhost:8084/lms/author",
-	GET_AUTHORS_BY_NAME: "http://localhost:8084/lms/authors/",
+	GET_ALL_AUTHORS: "http://54.185.38.228:8084/lms/authors",
+	DELETE_AUTHOR: "http://54.185.38.228:8084/lms/authors/",
+	UPDATE_AUTHOR: "http://54.185.38.228:8084/lms/authors/",
+	ADD_AUTHOR: "http://54.185.38.228:8084/lms/author",
+	GET_AUTHORS_BY_NAME: "http://54.185.38.228:8084/lms/authors/",
 	
 	
-	GET_ALL_BORROWERS: "http://localhost:8084/lms/borrowers",
-	DELETE_BORROWER: "http://localhost:8084/lms/borrowers/",
-	UPDATE_BORROWER: "http://localhost:8084/lms/borrowers/",
-	ADD_BORROWER: "http://localhost:8084/lms/borrower",
-	GET_BORROWERS_BY_NAME: "http://localhost:8084/lms/borrowers/name/",
+	GET_ALL_BORROWERS: "http://54.185.38.228:8084/lms/borrowers",
+	DELETE_BORROWER: "http://54.185.38.228:8084/lms/borrowers/",
+	UPDATE_BORROWER: "http://54.185.38.228:8084/lms/borrowers/",
+	ADD_BORROWER: "http://54.185.38.228:8084/lms/borrower",
+	GET_BORROWERS_BY_NAME: "http://54.185.38.228:8084/lms/borrowers/name/",
 	
-	GET_ALL_PUBLISHERS: "http://localhost:8084/lms/publishers",
-	DELETE_PUBLISHER: "http://localhost:8084/lms/publishers/",
-	UPDATE_PUBLISHER: "http://localhost:8084/lms/publishers/",
-	ADD_PUBLISHER: "http://localhost:8084/lms/publisher",
-	GET_PUBLISHERS_BY_NAME: "http://localhost:8084/lms/publishers/",
+	GET_ALL_PUBLISHERS: "http://54.185.38.228:8084/lms/publishers",
+	DELETE_PUBLISHER: "http://54.185.38.228:8084/lms/publishers/",
+	UPDATE_PUBLISHER: "http://54.185.38.228:8084/lms/publishers/",
+	ADD_PUBLISHER: "http://54.185.38.228:8084/lms/publisher",
+	GET_PUBLISHERS_BY_NAME: "http://54.185.38.228:8084/lms/publishers/",
 	
-	GET_ALL_BRANCHES: "http://localhost:8084/lms/libraryBranches",
-	DELETE_BRANCH: "http://localhost:8084/lms/libraryBranches/",
-	UPDATE_BRANCH: "http://localhost:8084/lms/libraryBranches/",
-	ADD_BRANCH: "http://localhost:8084/lms/libraryBranch",
-	GET_BRANCHES_BY_NAME: "http://localhost:8084/lms/libraryBranches/",
+	GET_ALL_BRANCHES: "http://54.185.38.228:8084/lms/libraryBranches",
+	DELETE_BRANCH: "http://54.185.38.228:8084/lms/libraryBranches/",
+	UPDATE_BRANCH: "http://54.185.38.228:8084/lms/libraryBranches/",
+	ADD_BRANCH: "http://54.185.38.228:8084/lms/libraryBranch",
+	GET_BRANCHES_BY_NAME: "http://54.185.38.228:8084/lms/libraryBranches/",
 	
-	GET_ALL_BOOKS: "http://localhost:8084/lms/books",
-	DELETE_BOOKS: "http://localhost:8084/lms/books/",
-	UPDATE_BOOK: "http://localhost:8084/lms/books/",
-	ADD_BOOK: "http://localhost:8084/lms/book",
-	GET_BOOKS_BY_NAME: "http://localhost:8084/lms/books/title/",
-	
-	
-	GET_ALL_GENRES: "http://localhost:8084/lms/genres",
-	DELETE_GENRES: "http://localhost:8084/lms/genres/",
-	UPDATE_GENRE: "http://localhost:8084/lms/genres/",
-	ADD_GENRE: "http://localhost:8084/lms/genre",
-	GET_GENRES_BY_NAME: "http://localhost:8084/lms/genres/",
-	
-	GET_ALL_LOANS: "http://localhost:8084/lms/bookLoans",
-	UPDATE_DUE_DATE: "http://localhost:8084/lms/bookLoans/dueDate",
-	GET_LOANS_BY_USERID: "http://localhost:8084/lms/admin/readBookLoansByUserId",
-	
-	GET_LOAN_BY_USER: "http://localhost:8084/lms/bookLoans/user/",
-	GET_BORROWER_BY_ID: "http://localhost:8084/lms/borrowers/",
-	RETURN_BOOK_LOAN: "http://localhost:8084/lms/bookLoans",
-	SAVE_BOOK_LOAN: "http://localhost:8084/lms/bookLoan",
+	GET_ALL_BOOKS: "http://54.185.38.228:8084/lms/books",
+	DELETE_BOOKS: "http://54.185.38.228:8084/lms/books/",
+	UPDATE_BOOK: "http://54.185.38.228:8084/lms/books/",
+	ADD_BOOK: "http://54.185.38.228:8084/lms/book",
+	GET_BOOKS_BY_NAME: "http://54.185.38.228:8084/lms/books/title/",
 	
 	
-	ADD_BOOK_COPY: "http://localhost:8084/lms/bookCopy",
-	UPDATE_BOOK_COPY: "http://localhost:8084/lms/bookCopies/",
-	GET_BOOK_COPY_BY_ID: "http://localhost:8084/lms/library/readBookCopiesById",
-	UPDATE_BOOK_COPY2: "http://localhost:8084/lms/bookCopies/return",
+	GET_ALL_GENRES: "http://54.185.38.228:8084/lms/genres",
+	DELETE_GENRES: "http://54.185.38.228:8084/lms/genres/",
+	UPDATE_GENRE: "http://54.185.38.228:8084/lms/genres/",
+	ADD_GENRE: "http://54.185.38.228:8084/lms/genre",
+	GET_GENRES_BY_NAME: "http://54.185.38.228:8084/lms/genres/",
+	
+	GET_ALL_LOANS: "http://54.185.38.228:8084/lms/bookLoans",
+	UPDATE_DUE_DATE: "http://54.185.38.228:8084/lms/bookLoans/dueDate",
+	GET_LOANS_BY_USERID: "http://54.185.38.228:8084/lms/admin/readBookLoansByUserId",
+	
+	GET_LOAN_BY_USER: "http://54.185.38.228:8084/lms/bookLoans/user/",
+	GET_BORROWER_BY_ID: "http://54.185.38.228:8084/lms/borrowers/",
+	RETURN_BOOK_LOAN: "http://54.185.38.228:8084/lms/bookLoans",
+	SAVE_BOOK_LOAN: "http://54.185.38.228:8084/lms/bookLoan",
+	
+	
+	ADD_BOOK_COPY: "http://54.185.38.228:8084/lms/bookCopy",
+	UPDATE_BOOK_COPY: "http://54.185.38.228:8084/lms/bookCopies/",
+	GET_BOOK_COPY_BY_ID: "http://54.185.38.228:8084/lms/library/readBookCopiesById",
+	UPDATE_BOOK_COPY2: "http://54.185.38.228:8084/lms/bookCopies/return",
 	
 	
 })
